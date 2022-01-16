54364eb9dd2c8f8210f468f13b772f4ced67994a
Cristián Romo <cristian.g.romo@gmail.com>
2022-01-16 12:33:53 -0600

    Add ADX detection and renaming

--- scripts/extract-adx.ps1
+++ scripts/extract-adx.ps1
@@ -1,4 +1,3 @@
-# $param1 = $args[0]
 param(
   $AwbDirectory = "C:\Program Files (x86)\Steam\steamapps\common\Tales of Symphonia\P12E\Data\TOS_Server\Rawdata\WIN",
   $AwbUnpacker = ".\vendor\awb-tools\AWB_unpacker.exe",
@@ -7,8 +6,30 @@ param(
 Write-Host $AwbDirectory
 New-Item $OutputDirectory -ItemType Directory -Force
 
+# Unpack the AWB files and move them to the output directory
 Get-ChildItem $AwbDirectory -Filter *.AWB | ForEach-Object {
   Write-Host "$OutputDirectory\$($_.FullName.Substring($AwbDirectory.Length + 1))"
   & $AwbUnpacker $_.FullName
   Move-Item "$($_)_extracted_files" -Destination "$OutputDirectory\$($_.FullName.Substring($AwbDirectory.Length + 1))"
+}
+
+function Test-IsAdxFile([string]$Path) {
+  $bytes = Get-Content $Path -AsByteStream -TotalCount 4
+  if (($bytes[0] -eq 0x80) -and ($bytes[1] -eq 0x00)) {
+    $copyrightOffset = (([int]$bytes[2] -shl 8) -bor [int]$bytes[3]) - 2
+    $bytes = Get-Content $Path -AsByteStream -TotalCount ($copyrightOffset + 6)
+    $copyright = [System.Text.Encoding]::ASCII.GetString($bytes[-6..-1])
+    if ($copyright -eq "(c)CRI") {
+      return $true
+    }
+  }
+  return $false
+}
+
+# Rename bin files to ADX if they match the magic numbers
+Get-ChildItem $OutputDirectory -Recurse -Filter *.bin | Where-Object {
+  Test-IsAdxFile $_
+} | ForEach-Object {
+  Write-Host $_
+  Move-Item $_ -Destination "$([System.Io.Path]::GetDirectoryName($_))\$([System.Io.Path]::GetFileNameWithoutExtension($_)).adx"
 }
