150c3558b655a6be78123d367d89ca5f1f5181c2
Cristián Romo <cristian.g.romo@gmail.com>
2022-05-18 16:33:12 -0500

    Clean up audio file index

--- public/.init.lua
+++ public/.init.lua
@@ -1,8 +1,24 @@
 local json = require "dkjson"
+require "fun"()
 
 print("Hey there, this is lua")
 
 local audioIndexFile = io.open(".\\data\\audio\\index.json")
 local audioFiles = json.decode(audioIndexFile:read("*a"))
 audioIndexFile:close()
-for i, value in ipairs(audioFiles) do print(i, value) end
+
+function posixPathFromWindowsPath(path)
+  path = path:gsub("\\", "/")
+  return path
+end
+
+function removePrefix(prefixPattern)
+  return function(str)
+    str = str:gsub("^" .. prefixPattern, "")
+    return str
+  end
+end
+
+audioFiles = totable(map(removePrefix("./data"), map(posixPathFromWindowsPath, audioFiles)))
+
+each(print, take(4, audioFiles))
